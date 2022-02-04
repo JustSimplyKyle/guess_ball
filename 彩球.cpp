dd37bde4e93022168bcaaf2c5a159cf8e63e10e4
KyleUltimate <shiue.kyle@gmail.com>
2022-02-04 18:07:04 +0800

    Code Refactor.

--- 彩球.cpp
+++ 彩球.cpp
@@ -2,82 +2,82 @@
 #include <vector>
 #include <random>
 #include <algorithm>
+
+using namespace std;
+
 std::random_device rd;
 std::default_random_engine gen = std::default_random_engine(rd());
-void print(std::vector<int> num){
-    for(int i = 1; i <= num.size(); ++i){
-        std::cout << "Number " << i << ":\t" << num[i-1] << std::endl;
+
+vector<int> generateUniqueRandomArray(int n, int rangeL, int rangeR) {
+    vector<int> arr(n);
+    std::uniform_int_distribution<int> dis(rangeL, rangeR);
+    int random;
+    for (int i = 0; i < n; i++) {
+        do {
+            random = dis(gen);
+        } while (std::find(arr.begin(), arr.end(), random) != arr.end());
+        arr[i] = random;
     }
+    return arr;
 }
-void input(std::vector<int> &num,int times){
-    int temp;
-    for(int i = 1; i <= times; ++i){
-        std::cout << "Number " << i << ":\t";
-        std::cin >> temp;
-        num.push_back(temp);
+
+int main() {
+    cout << "彩球遊戲" << endl;
+    vector<int> answer;
+    int size;
+    cout << "how many balls do you want to play?" << endl;
+    cin >> size;
+    cout << "what are the balls' range?" << endl;
+    int rangeL, rangeR;
+    cin >> rangeL >> rangeR;
+    //check if range is valid
+    if (rangeL > rangeR) {
+        cerr << "range is invalid" << endl;
+        return 0;
     }
-}
-void RNG_answer_creation(std::vector<int> &number, int times){
-    std::uniform_int_distribution<int> dis(0,10);
-    int temp_RNG;
-    temp_RNG=dis(gen);
-    number.push_back(temp_RNG);
-    bool finding=true;
-    bool first=true;
-    auto num_it=number.begin();
-    auto find_number_it=find(number.begin(),number.end(),temp_RNG);
-    for(int i = 0;i<times;++i){
-        while(finding==true){
-            if(find_number_it!=number.end() && first != true){
-                temp_RNG=dis(gen);
-                find_number_it=find(number.begin(),number.end(),temp_RNG);
-            }
-            else{
-                *num_it=temp_RNG;
-                finding=false;
-            }
-        }
-        if(i!=times-1){
-            temp_RNG=dis(gen);
-            auto temp_it=find(number.begin(),number.end(),temp_RNG);
-            if(temp_it==number.end()){
-                number.push_back(temp_RNG);
-                find_number_it=number.end();
-            }
-            else{
-                number.push_back(temp_RNG);
-            }
-            num_it=number.end()-1;
-            finding=true;
-            first=false;
+    //check if range is smaller than size
+    if (rangeR - rangeL + 1 < size) {
+        cerr << "range is too small" << endl;
+        return 0;
+    }
+    answer = generateUniqueRandomArray(size, rangeL, rangeR);
+    printf("User input \n(%d~%d)\n", rangeL, rangeR);
+    vector<int> input(size);
+    for (int i = 0; i < size; i++) {
+        cin >> input[i];
+    }
+    //check if the input's sizes are valid
+    for (int i = 0; i < size; i++) {
+        if (input[i] < rangeL || input[i] > rangeR) {
+            cerr << "input error" << endl;
+            return 0;
         }
     }
-}
-std::vector<int> testequals(std::vector<int> &answer,std::vector<int> &check){
-    std::vector<int> correct;
-    for(int i=0;i<answer.size();++i){
-        if(answer[i]==check[i]){
-            correct.push_back(i);
+    //check if there are duplicate numbers in inputs
+    vector<int> temp_check;
+    temp_check = input;
+    sort(temp_check.begin(), temp_check.end());
+    if (std::adjacent_find(temp_check.begin(), temp_check.end()) != temp_check.end()) {
+        cerr << "Duplicate numbers are not allowed." << endl;
+        return 0;
+    }
+    //compare input and answer
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        if (input[i] == answer[i]) {
+            count++;
         }
     }
-    return correct;
-}
-int main(){
-    std::vector<int> number;
-    std::vector<int> input_num;
-    int times=3;
-    RNG_answer_creation(number,times);
-    //std::cout<<"Answer\n";
-    //print(number);
-    std::cout<<"Input\n";
-    input(input_num,times);
-    std::vector<int> correct=testequals(number,input_num);
-    for(int i=0;i<correct.size();++i){
-        std::cout<< "Number " << (correct[i])+1 << " guess correctly!\n";
-        std::cout<< "The number you have guessed is " << number[correct[i]] << std::endl;
+    cout << "You got " << count << " numbers right." << endl;
+    //print all answers
+    cout << "Answer: ";
+    for (int i = 0; i < size; i++) {
+        cout << answer[i] << " ";
     }
-    if(correct.empty()){
-        std::cout<<"Every number you have guess is incorrect!\n";
+    //print input
+    cout << "\nInput: ";
+    for (int i = 0; i < size; i++) {
+        cout << input[i] << " ";
     }
     return 0;
 }
